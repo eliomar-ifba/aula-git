2c1f4e86f3997fc9846839c52cb6407b0ac2e69b
eliomar-ifba <177131835+eliomar-ifba@users.noreply.github.com>
2025-08-02 08:48:27 -0300

    Commit 3 em branch2

--- src/App.java
+++ src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Commit 2");
+        System.out.println("Commit 2 em branch2");
     }
 }
